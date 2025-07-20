f9652b79b22472c1f6f91e4063d9c55888f5780c
Manasa <manasa.7m@gmail.com>
2025-07-20 18:48:27 -0400

    updated path

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use('/api/users', router)
 
 app.get('/', (req,res) => {
-    res.status(201).json("Login API");
+    res.status(201).json("Innovate Inc.");
 })
 
 app.listen(PORT, () => {
